4bdf6656b5188f2618e6dfd10163622fc600a5a3
gizmond <lukas-yu@hotmail.com>
2019-05-13 18:24:42 +0200

    Revert "Abgabe"
    This reverts commit 79124bb945215cabfb354f09f62adf702366b7d9.

--- Abgabe/processing_scripts/reverse.py
+++
@@ -1,8 +0,0 @@
-import codecs
-import os
-
-with codecs.open("test.bpe.de", mode="r", encoding="utf-8") as source:
-    with codecs.open("test.bpe.reversed", mode="w", encoding="utf-8") as target:
-        for line in source:
-            reversed_list = line.split()[::-1]
-            target.write(" ".join(reversed_list) + "\n")
